24983e67e3d632bc01f7795b6fba14be2622025d
Victor <victor@ronnerstedt.com>
2022-06-06 23:36:36 +0200

    toggle like and drunk

--- src/Pages/Beer.tsx
+++ src/Pages/Beer.tsx
@@ -1,6 +1,13 @@
-import { gql, useQuery } from '@apollo/client'
-import { Avatar, Paper, Stack, Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import { gql, useMutation, useQuery } from '@apollo/client'
+import {
+  Avatar,
+  Paper,
+  Stack,
+  Typography,
+  Box,
+  IconButton
+} from '@mui/material'
+import { Favorite, CheckRounded } from '@mui/icons-material'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
@@ -13,6 +20,21 @@ const StyledImage = styled('img')`
   width: 100%;
   height: 100%;
   object-fit: contain;
+`
+
+const TOGGLE_LIKE = gql`
+  mutation Mutation($input: CreateLikeInput) {
+    like {
+      toggleDrunk(input: $input)
+    }
+  }
+`
+const TOGGLE_DRUNK = gql`
+  mutation Drunk($input: CreateDrunkInput) {
+    drunk {
+      toggleDrunk(input: $input)
+    }
+  }
 `
 
 const BEER_QUERY = gql`
@@ -47,6 +69,22 @@ const Beer = () => {
     }
   })
 
+  const [toggleLike] = useMutation(TOGGLE_LIKE, {
+    variables: {
+      input: {
+        beerId
+      }
+    }
+  })
+
+  const [toggleDrunk] = useMutation(TOGGLE_DRUNK, {
+    variables: {
+      input: {
+        beerId
+      }
+    }
+  })
+
   const beer = data?.beer.beerQuery
 
   if (!beer) return null
@@ -73,9 +111,21 @@ const Beer = () => {
             marginLeft: isLarge ? 4 : 0
           }}
         >
-          <Typography variant='h2'>{beer?.name}</Typography>
-          <Typography variant='overline'>{beer?.type}</Typography>
-          <Typography variant='h4'>{beer?.alcohol} %</Typography>
+          <Box display='flex' alignItems='start'>
+            <Stack sx={{ marginRight: 2 }}>
+              <Typography variant='h2'>{beer?.name}</Typography>
+              <Typography variant='overline'>{beer?.type}</Typography>
+              <Typography variant='h4'>{beer?.alcohol} %</Typography>
+            </Stack>
+            <Stack direction='row'>
+              <IconButton onClick={() => toggleLike()}>
+                <Favorite sx={{ fontSize: '1.5em' }} />
+              </IconButton>
+              <IconButton onClick={() => toggleDrunk()}>
+                <CheckRounded sx={{ fontSize: '1.5em' }} />
+              </IconButton>
+            </Stack>
+          </Box>
           <Stack direction={isLarge ? 'row' : 'column'} spacing={2} marginY={2}>
             {beer.containers?.map(x => (
               <Paper
